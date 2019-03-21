95a651ec8cb0f46a2f119ea7d5a07f7856ec8326
Calvin McLean <calvinmclean@email.arizona.edu>
2019-03-21 12:48:18 -0700

    Combinbe add_corgi and list_corgi

--- corgi_store/views.py
+++ corgi_store/views.py
@@ -18,28 +18,24 @@ def buy_corgi(request):
     return render(request, 'browse.html', {'title': 'Buy a Corgi', 'corgis': Corgi.objects.all()})
 
 def list_corgi(request):
+    if request.method == "POST":
+        user = request.user
+        params = request.POST
+        price = params['price'] if 'price' in params else None
+        corgi = Corgi.objects.create(
+            name=params['name'],
+            age_years=params['years'],
+            age_months=params['months'],
+            gender=params['gender'],
+            coloring=params['coloring'],
+            city=params['city'],
+            state=params['state'],
+            owner=user,
+            description=params['description']
+        )
+        return redirect('browse_corgis')
     return render(request, 'list_corgi.html')
 
-def add_corgi(request):
-    print("POST =", request.POST)
-    user = get_object_or_404(User, pk=1)
-    # user = request.user
-    params = request.POST
-    price = params['price'] if 'price' in params else None
-    corgi = Corgi.objects.create(
-        name=params['name'],
-        age_years=params['years'],
-        age_months=params['months'],
-        gender=params['gender'],
-        coloring=params['coloring'],
-        city=params['city'],
-        state=params['state'],
-        owner=user,
-        description=params['description']
-    )
-    return HttpResponse("Successfully added Corgi\n{}\n".format(corgi))
-    # curl --data "name=spooky&age=3&gender=M&coloring=sable&location=tucson&description=cool&price=69000" http://127.0.0.1:8000/create_corgi/
-    
 def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
